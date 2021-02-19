b21154e1b81afa8d9d7c2a559badebef678ee507
zgy512 <15003739704@163.com>
2021-02-20 01:55:26 +0800

    add brand

--- web_manage/src/main/java/cn/itcast/core/controller/BrandController.java
+++ web_manage/src/main/java/cn/itcast/core/controller/BrandController.java
@@ -1,5 +1,6 @@
 package cn.itcast.core.controller;
 
+import cn.itcast.core.pojo.entity.PageResult;
 import cn.itcast.core.pojo.entity.Result;
 import cn.itcast.core.pojo.good.Brand;
 import com.alibaba.dubbo.config.annotation.Reference;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * 商品控制器
@@ -20,51 +22,78 @@ import java.util.List;
 public class BrandController {
     @Reference
     private BrandService brandService;
+
     @RequestMapping("/findAll")
-    public List<Brand> findAll(){
+    public List<Brand> findAll() {
         List<Brand> brandList = brandService.findAll();
         return brandList;
     }
 
+    @RequestMapping("/findByPage")
+    public PageResult findByPage(@RequestBody Brand brand, Integer page, Integer rows) {
+        try {
+            PageResult search = brandService.search(brand, page, rows);
+            return search;
+        }catch (Exception e){
+            e.printStackTrace();
+            return new PageResult("fail",0L,null);
+        }
+    }
+
     @RequestMapping("/findOne")
-    public Brand findOne(Long id){
+    public Brand findOne(Long id) {
         Brand one = brandService.findOne(id);
         return one;
     }
 
     @RequestMapping("/add")
-    public Result addBrand(@RequestBody Brand brand){
+    public Result addBrand(@RequestBody Brand brand) {
         //商品添加
         try {
             brandService.add(brand);
-            return new Result(true,"ok");
-        }catch (Exception e){
-            return new Result(false,"fail");
+            return new Result(true, "ok");
+        } catch (Exception e) {
+            return new Result(false, "fail");
         }
     }
+
     @RequestMapping("/delete")
-    public Result deleteBrand(List list){
+    public Result deleteBrand(Long[] ids) {
         //商品删除
         try {
-            brandService.delete(list);
-            return new Result(true,"delete ok");
-        }catch (Exception e){
-            return new Result(false,"delete fail");
+            brandService.delete(ids);
+            return new Result(true, "delete ok");
+        } catch (Exception e) {
+            return new Result(false, "delete fail");
         }
     }
+
     @RequestMapping("/update")
-    public Result updateBrand(@RequestBody Brand brand){
+    public Result updateBrand(@RequestBody Brand brand) {
         //商品修改
         try {
             brandService.update(brand);
-            return new Result(true,"update ok");
-        }catch (Exception e){
-            return new Result(false,"update fail");
+            return new Result(true, "update ok");
+        } catch (Exception e) {
+            return new Result(false, "update fail");
         }
     }
-    @RequestMapping("/test")
-    public String test(){
-        System.out.println("hello");
-        return "hello World";
+
+    @RequestMapping("/search")
+    public PageResult search(@RequestBody Brand brand, Integer page, Integer rows) {
+        try {
+            PageResult search = brandService.search(brand, page, rows);
+            return search;
+        }catch (Exception e){
+            e.printStackTrace();
+            return new PageResult("fail",0L,null);
+        }
     }
+
+    @RequestMapping("/selectOptionList")
+    public List<Map> selectOptionList() {
+        List<Map>  maps= brandService.selectOptionList();
+        return maps;
+    }
+
 }
--- web_manage/src/main/java/cn/itcast/core/controller/SpecificationController.java
+++ web_manage/src/main/java/cn/itcast/core/controller/SpecificationController.java
@@ -1,13 +1,85 @@
 package cn.itcast.core.controller;
 
+import cn.itcast.core.pojo.entity.PageResult;
+import cn.itcast.core.pojo.entity.Result;
+import cn.itcast.core.pojo.entity.SpecEntity;
+import cn.itcast.core.pojo.specification.Specification;
+import cn.itcast.core.service.SpecifitionService;
+import com.alibaba.dubbo.config.annotation.Reference;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * 规格的控制器
  */
 
 @RestController
+@RequestMapping("/specification")
 public class SpecificationController {
+    @Reference
+    private SpecifitionService specifitionService;
 
+    @RequestMapping("/findAll")
+    public List<Specification> findAll() {
+        List<Specification> specificationList = specifitionService.findAll();
+        return specificationList;
+    }
+
+    @RequestMapping("/findPage")
+    public PageResult findPage(@RequestBody Specification specification, Integer page, Integer rows) {
+        PageResult pageResult = specifitionService.search(specification, page, rows);
+        return pageResult;
+    }
+
+    @RequestMapping("/findOne")
+    public SpecEntity findOne(Long id) {
+        SpecEntity specEntity = specifitionService.findOne(id);
+        return specEntity;
+    }
+
+    @RequestMapping("/add")
+    public Result addSpecification(@RequestBody SpecEntity specEntity) {
+        try {
+            specifitionService.add(specEntity);
+            return new Result(true, "添加成功");
+        } catch (Exception e) {
+            return new Result(false, "添加失败");
+        }
+    }
+
+    @RequestMapping("/update")
+    public Result updateSpecification(@RequestBody SpecEntity specEntity) {
+        try {
+            specifitionService.update(specEntity);
+            return new Result(true, "更新成功");
+        } catch (Exception e) {
+            return new Result(false, "更新失败");
+        }
+    }
+
+    @RequestMapping("/delete")
+    public Result deleteSpecification(Long[] ids) {
+        try {
+            specifitionService.delete(ids);
+            return new Result(true, "删除成功");
+        } catch (Exception e) {
+            return new Result(false, "删除失败");
+        }
+    }
+
+    @RequestMapping("/search")
+    public PageResult search(@RequestBody Specification specification, Integer page, Integer rows) {
+        PageResult pageResult = specifitionService.search(specification, page, rows);
+        return pageResult;
+    }
+
+    @RequestMapping("/selectOptionList")
+    public List<Map> selectOptionList() {
+        List<Map> maps = specifitionService.selectOptionList();
+        return maps;
+    }
 }
